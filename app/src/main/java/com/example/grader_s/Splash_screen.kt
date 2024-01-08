a4f378c0c61248c7a0ea9eb92e19a39cd63573ba
Yugant verma <46397122+captain196@users.noreply.github.com>
2024-01-08 06:44:59 +0530

    Splash_screen

---
+++ app/src/main/java/com/example/grader_s/Splash_screen.kt
@@ -0,0 +1,22 @@
+package com.example.grader_s
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            // Start the main activity after the splash time out
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Close this activity
+            finish()
+        }, 3000)
+    }
+}
